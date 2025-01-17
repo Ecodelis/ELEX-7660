b785e158d79e30acacfe3637b1be1aff9e06349b
Marcus Fu <38443839+Ecodelis@users.noreply.github.com>
2025-01-17 11:00:07 -0800

    Added an example file for code formating and standards for my labs

---
+++ format_examples/upDownCount.sv
@@ -0,0 +1,12 @@
+// File: upDownCount.sv
+// Description: Simple state machine that counts up or down 
+//              based on the input signal upDown.
+// Author: Marcus Fu
+// Date: 2025-01-17
+
+module upDownCnt3 ( input logic upDown
+                    output logic [2:0] count,
+                    input logic clk) ;
+
+
+endmodule
