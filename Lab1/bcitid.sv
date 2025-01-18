04553693e9d3f2bba8a338242cb6bfa5eb74a8b0
Marcus Fu <38443839+Ecodelis@users.noreply.github.com>
2025-01-18 15:04:00 -0800

    Formatted and commented lab1 files Modified: - bcitid.sv - decode2.sv - decode7.sv

--- Lab1/bcitid.sv
+++ Lab1/bcitid.sv
@@ -1,14 +1,20 @@
+// File: bcitid.sv
+// Description: A 4x4 bit memory that will store the last 
+// 				 four digits of my BCIT ID
+// Author: Marcus Fu
+// Date: 2025/01/17
+
 module bcitid ( input logic [1:0] digit, 		// digit counter
 					output logic [3:0] idnum );	// studentID num for each digit
 
-	logic [3:0] myID [3:0] = '{4'h1, 4'h4, 4'h6, 4'h8};
+	logic [3:0] myID [3:0] = '{4'h1, 4'h4, 4'h6, 4'h8}; // Stores last 4 numbers of the student ID in hexcode
  
 	always_comb
-		case (digit)
-			2'b00 : idnum = myID[0]; 	// Seg 1
-			2'b01 : idnum = myID[1];	// Seg 2
-			2'b10 : idnum = myID[2];	// Seg 3
-			2'b11 : idnum = myID[3];	// Seg 4
+		case (digit) // Displays ID number corresponding to the active digit
+			2'b00 : idnum = myID[0]; 
+			2'b01 : idnum = myID[1];
+			2'b10 : idnum = myID[2];
+			2'b11 : idnum = myID[3];
 		endcase
 
 endmodule
--- Lab1/decode2.sv
+++ Lab1/decode2.sv
@@ -1,3 +1,10 @@
+// File: decode2.sv
+// Description: A 2-to-4 decoder. Controls the digit to be displayed.
+// Author: Marcus Fu
+// Date: 2025/01/17
+
+
+
 module decode2 ( input logic [1:0] digit, // digit counter
 					  output logic [3:0] ct ); // active low digit enable
 
