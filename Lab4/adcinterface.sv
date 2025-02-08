b3524cd94c11042c6a34f5310bf1b5673b09ef7c
Marcus Fu <38443839+Ecodelis@users.noreply.github.com>
2025-02-08 02:41:14 -0800

    Rewrote adcinterface.sv

--- Lab4/adcinterface.sv
+++ Lab4/adcinterface.sv
@@ -1,33 +1,92 @@
-`define CYCLE_12 12 
-`define CYCLE_1 1
-
 module adcinterface(
-    input logic clk, reset_n, // clock (1.560 MHz) and reset
-    input logic [2:0] chan, // ADC channel to sample
-    output logic [11:0] result, // ADC result
+    input  logic        clk,       // clock (1.560 MHz) and reset
+    input  logic        reset_n,   // active-low reset
+    input  logic [2:0]  chan,      // ADC channel to sample
+    output logic [11:0] result,    // ADC result
     
-    // ltc2308 signals
-    output logic ADC_CONVST, ADC_SCK, ADC_SDI,
-    input logic ADC_SDO );
+    // LTC2308 signals
+    output logic ADC_CONVST, 
+    output logic ADC_SCK, 
+    output logic ADC_SDI,
+    input  logic ADC_SDO
+);
 
-    enum logic [1:0] { 
-        H,      // ADC_CONVST HIGH
-        L,      // ADC_CONVST LOW
-        WAIT,   // 12 CYCLES
-        STOP    // ADC_SCL ONE CYCLE STOP TOGGLE
-        } state, next_state;
-    logic [3:0] count, next_count; // count logic for cycle control
-    logic [3:0] shift_index, next_shift_index; // shift index for shifting the word into ADC_SDI
+    enum logic [1:0] {
+        INIT_CONV,     // Initialize conversion (e.g., assert ADC_CONVST)
+        LOW_CONV,      // Conversion signal low
+        DATA_TRANSFER, // Data transfer state
+        PAUSE_SCK      // Pause state for SCK
+    } state, next_state;
 
-    logic [11:0] config_word; // 12-bit configuration word to ADC
-    logic [2:0] address_select; // 3-bit address select (Assuming single-ended mode)
-    logic OS; // Odd/Sign Bit
+    // State Machine Signals
+    logic [3:0] delay, next_delay;  // Delay counter
 
-    assign ADC_SCK = (state == WAIT) ? clk : 0;
 
+    // ADC Interface Signals
+    logic [5:0] config_word;  // 6-bit Configuration word for ADC
+    logic [2:0] address_select;  // Address select for ADC (assuming single-ended mode)
+    logic [2:0] shift_count;  // Shift counter for ADC_SDI
+    
+
+//---------------------------------------------------------
+// --- STATE MACHINE LOGIC SECTION ---
+//---------------------------------------------------------
+
+    // On every posedge clk, if delay reaches 0, update state and reload delay.
+    always_ff @(posedge clk) begin
+        if (!reset_n) begin
+            state <= INIT_CONV;
+            delay <= 1;
+            ADC_CONVST <= 1;
+            ADC_SDI <= 0;
+            result <= 0;
+            shift_count <= 6; // Load shift count for ADC_SDI
+        end else begin
+            if (delay == 0) begin
+                state <= next_state; // Update state
+                delay <= next_delay; // Load in delay for the next state
+            end else begin
+                delay <= delay - 1;
+            end
+        end
+    end
+
+    // Next State Combinational Logic
+    always_comb begin
+        // Default next_state assignment
+        next_state = state;
+
+        case (state)
+            INIT_CONV: begin
+                next_state = LOW_CONV;
+                next_delay = 1;  // Load 1 cycle delay for the next state
+            end
+            LOW_CONV: begin
+                next_state = DATA_TRANSFER;
+                next_delay = 12;  // Load 12 cycle delay for the next state
+            end
+            DATA_TRANSFER: begin
+                next_state = PAUSE_SCK;
+                next_delay = 1;  // Load 1 cycle delay for the next state
+            end
+            PAUSE_SCK: begin
+                next_state = INIT_CONV;
+                next_delay = 1;  // Load 1 cycle delay for the next state
+            end
+            default: next_state = INIT_CONV;
+        endcase
+    end
+
+//---------------------------------------------------------
+// --- ADC INTERFACE LOGIC SECTION ---
+//---------------------------------------------------------
+
+    assign ADC_SCK = (state == DATA_TRANSFER) ? clk : 0; // SCK is high during DATA_TRANSFER state
+
+    // Address Select Combinational Logic
     always_comb begin
         // chan2address (assumes single-ended mode)
-        unique case(chan)
+        case(chan)
             3'b000: address_select = 3'b000; // CH0
             3'b001: address_select = 3'b100; // CH1
             3'b010: address_select = 3'b001; // CH2
@@ -36,89 +95,45 @@ module adcinterface(
             3'b101: address_select = 3'b110; // CH5
             3'b110: address_select = 3'b011; // CH6
             3'b111: address_select = 3'b111; // CH7
+            default: address_select = 3'b000; // Default to CH0
         endcase
-
-        if (count > 0) begin
-            next_count = count - 1;
-            next_state = state;
-            
-        end else begin
-            unique case (state)
-                H: begin
-                    next_state = L;
-                    next_count = `CYCLE_1;
-                end
-                L: begin
-                    next_state = WAIT;
-                    next_count = `CYCLE_1;
-                end
-                WAIT: begin
-                    next_state = STOP;
-                    next_count = `CYCLE_12;
-                end
-                STOP: begin
-                    next_state = H;
-                    next_count = `CYCLE_1;
-                end
-            endcase
-        end
-
-        // Control next_shift_index based on current state
-        if (state == WAIT && shift_index > 0) begin
-            next_shift_index = shift_index - 1;  // Decrement shift_index after sending the bit
-        end else if (state == L) begin
-            next_shift_index = 11; // Reset shift index before shifting starts
-        end else begin
-            next_shift_index = shift_index; // Maintain current shift_index
-        end
     end
+
+
 
     always_ff @(posedge clk) begin
-        if (~reset_n) begin
-            count <= 1;
-            result <= 0;
-            state <= H;
-            ADC_SDI <= 0;
-            ADC_CONVST <= 0;
-            shift_index <= 11; // Initialize shift_index to 11
-        end else begin
-            count <= next_count;
-            state <= next_state;
-            shift_index <= next_shift_index; // Update shift_index only once per clock cycle
+        if (state == LOW_CONV) begin
+            shift_count <= 6; // Load shift count for ADC_SDI
+        end
 
-            unique case(state)
-                H: begin
-                    ADC_CONVST <= 1;
-                end
-                L: begin
-                    ADC_CONVST <= 0;
+        else if (state == DATA_TRANSFER) begin
+            // 6-bit configuration word
+            // S/D = Single-Ended/Differential Bit
+            // O/S = Odd/Sign Bit (acts as extra address bit in single-ended mode)
+            // S1 = Address Select Bit 1
+            // S0 = Address Select Bit 0
+            // UNI = Unipolar/Bipolar Bit
+            // SLP = Sleep Mode Bit
+            // Filler 0 Bits
+            config_word <= {1'b1, address_select, 1'b0, 1'b0}; // Load in config before negedge
 
-                    // 12-bit configuration word
-                    config_word <= {1'b1, address_select, 1'b0, 1'b0, 6'b000000};
-                    // S/D = Single-Ended/Differential Bit
-                    // O/S = Odd/Sign Bit (acts as extra address bit in single-ended mode)
-                    // S1 = Address Select Bit 1
-                    // S0 = Address Select Bit 0
-                    // UNI = Unipolar/Bipolar Bit
-                    // SLP = Sleep Mode Bit
-                    // Filler 0 Bits
-                end
-                WAIT: begin
-                    // Shift in the ADC_SDO data every posedge
-                    result <= { result[10:0], ADC_SDO };
-                end
-                STOP: begin
-                    ADC_CONVST <= 0;
-                end
-            endcase
+            // Shift in data from ADC_SDO
+            result <= {ADC_SDO, result[11:1]};
         end
     end
 
-    // Shift the configuration word out on ADC_SDI (falling edge of ADC_SCK)
     always_ff @(negedge clk) begin
-        if (state == WAIT && shift_index >= 0) begin
-            ADC_SDI <= config_word[shift_index];  // Shift out one bit
+        if (state == DATA_TRANSFER) begin
+            if (shift_count >= 0) begin
+            // Shift in address and start conversion from MSB to LSB
+            ADC_SDI <= config_word[shift_count];
+
+            // Decrement shift count (next cycle)
+            shift_count <= shift_count - 1;
+            end else begin
+                ADC_SDI <= 0; // Load in 0 after all bits have been shifted
+            end
+
         end
     end
-
 endmodule
